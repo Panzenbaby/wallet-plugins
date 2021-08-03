221f3fa59b00f8d0cfcbd14d9c8642be118ddba3
Edgar Goetzendorff <hello@dated.fun>
2021-08-03 16:45:32 +0200

    chore: typescript, eslint and prettier

--- ark-explorer/src/index.tsx
+++ ark-explorer/src/index.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 
 const explorerUrls = {
-	"Mainnet": "https://explorer.ark.io/",
-	"Devnet": "https://dexplorer.ark.io/",
+	Devnet: "https://dexplorer.ark.io/",
+	Mainnet: "https://explorer.ark.io/",
 };
 
-export default (api) => {
+const entry = (api) => {
 	const App = () => {
 		const [selectedLabel, setSelectedLabel] = React.useState("Mainnet");
 
 		return (
 			<section className="flex-1 border-t-3 border-theme-danger-500 bg-theme-neutral-200 flex flex-col justify-center">
 				<div className="flex items-center justify-center divide-x divide-theme-secondary-600 py-4 shadow-md z-20">
-					{Object.keys(explorerUrls).map((explorerLabel, index) => (
+					{["Mainnet", "Devnet"].map((explorerLabel, index) => (
 						<button
 							key={index}
 							className={`px-4 py-2 w-30 ${selectedLabel === explorerLabel ? "font-bold" : ""}`}
@@ -23,10 +23,12 @@ export default (api) => {
 					))}
 				</div>
 
-        <webview className="w-full h-full" src={explorerUrls[selectedLabel]} />
+				<webview className="w-full h-full" src={explorerUrls[selectedLabel]} />
 			</section>
 		);
 	};
 
 	api.launch().render(<App />);
 };
+
+export default entry;
